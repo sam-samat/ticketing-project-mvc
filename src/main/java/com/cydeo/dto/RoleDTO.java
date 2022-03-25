faa3582345754b0cb1788e13ea95e672831ad2be
aidoka888 <bluewalnut996@gmail.com>
2022-03-24 20:27:03 -0500

    initial setup

---
+++ src/main/java/com/cydeo/dto/RoleDTO.java
@@ -0,0 +1,16 @@
+package com.cydeo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDTO {
+
+    private String id;
+    private String description;
+}
